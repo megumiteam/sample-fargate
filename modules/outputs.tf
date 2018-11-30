86203f1fa3e3f60a92f8b236c7405f7139eecb4d
wokamoto <wokamoto@digitalcube.jp>
2018-11-30 11:07:51 +0900

    fixed typo

--- modules/outputs.tf
+++ modules/outputs.tf
@@ -4,11 +4,11 @@ output "vpc_id" {
 output "subnets_id" {
     value = "${aws_subnet.subnets.*.id}"
 }
-output "iam_role_id" {
-    value = "${aws_iam_role.iam_role.id}"
+output "iam_role_task_id" {
+    value = "${aws_iam_role.ecs_task.id}"
 }
 output "iam_role_execution_id" {
-    value = "${aws_iam_role.execution.id}"
+    value = "${aws_iam_role.ecs_execution.id}"
 }
 output "ecs_arn" {
     value = "${aws_ecs_cluster.ecs.arn}"
